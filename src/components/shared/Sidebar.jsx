cd69305158c1365541421885caa540fcbd9b1fe6
Martin Ahindura <martinahindura@gmail.com>
2017-09-15 20:26:19 +0300

    Add sidebar on mobile view

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const Sidebar = props => (
-  <div className="lay_col on_small_0 on_medium_3 on_large_2 sidebar">{props.children}</div>
-);
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { expanded: false };
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+  }
+
+  handleBtnClick(e) {
+    const btn = e.target;
+    this.setState(prevState => ({ expanded: !prevState.expanded }));
+    console.log(this.state.expanded, btn.innerHTML);
+    btn.innerHTML = btn.innerHTML === '&gt;' ? '&lt;' : '&gt;';
+  }
+
+  render() {
+    const classForDisplay = this.state.expanded
+      ? 'sidebar_overlayed sidebar'
+      : 'lay_col on_small_2 on_medium_3 on_large_2 sidebar';
+    return (
+      <div className={classForDisplay}>
+        <button id="more-button" onClick={this.handleBtnClick}>
+          &gt;
+        </button>
+        {this.props.children}
+      </div>
+    );
+  }
+}
 
 Sidebar.propTypes = {
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.arrayOf(PropTypes.node)]).isRequired,

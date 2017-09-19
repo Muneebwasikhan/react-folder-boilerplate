0d230f97a5b448b23da2587df7856f11b4c84e44
Martin Ahindura <martinahindura@gmail.com>
2017-09-19 11:30:22 +0300

    Update the text on HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,7 +5,7 @@ const HomePage = () => (
   <div className="App">
     <div className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to React</h2>
+      <h2>Welcome to React Folder Boilerplate</h2>
     </div>
     <p className="App-intro">
       To get started, edit <code>src/App.js</code> and save to reload.
